5ce6e758b696bb176918d0d35d0b2e63bc90462f
GhufranRifaldi <gufran.rivaldi@gmail.com>
2022-04-29 22:10:22 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './component/Header.js';
+import Hero from './component/Hero.js';
+
+
+function App() {
+    return (
+        <div>
+        <Header />
+        <Hero />
+        </div>
+
+    );
+}
+export default App;
